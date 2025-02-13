010c789dae8cacaaf477a8f8a006a04b2f1700e3
Adam Kaufman <akaufman@kth.se>
2025-02-13 16:55:18 +0100

    creating frontend folder

---
+++ backend/backend/src/main/java/com/example/backend/MovieController.java
@@ -0,0 +1,22 @@
+package com.example.backend;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@RestController
+public class MovieController {
+    
+}
