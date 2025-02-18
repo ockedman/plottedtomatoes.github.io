563a1797900102d447c2ad841b081fb1e61b7aea
Adam Kaufman <akaufman@kth.se>
2025-02-18 12:03:22 +0100

    semi-functioning backend

--- backend/backend/src/main/java/com/example/backend/LoadDatabase.java
+++ backend/backend/src/main/java/com/example/backend/LoadDatabase.java
@@ -1,12 +1,177 @@
 package com.example.backend;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.loader.ast.spi.Loadable;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import aj.org.objectweb.asm.TypeReference;
+
 @Configuration
-public class LoadDatabase {
+public class LoadDatabase implements CommandLineRunner {
+
+    private final MovieService movieService;
+    private final ObjectMapper objectMapper;
+
+
+    public MovieService getMovieService() {
+        return this.movieService;
+    }
+
+    public ObjectMapper getObjectMapper() {
+        return this.objectMapper;
+    }
+
+
+    public LoadDatabase(MovieService movieService, ObjectMapper objectMapper) {
+        this.movieService = movieService;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        /*
+        JsonNode json;
+
+        try(InputStream inputStream = TypeReference.class.getResourceAsStream("movies.json")) {
+            json = objectMapper.readTree(inputStream, JsonNode.class);
+        } catch(IOException e) {
+            throw new RuntimeException("failed to read JSON data", e);
+        }
+
+        List<Movie> movies = new ArrayList<>():
+        for (JsonNode node : json.get("movies")) {
+            MovieDTO movieDTO = objectMapper.treeToValue(node, MovieDTO.class);
+            Movie movie = new Movie(movieDTO);
+            movies.add(movie);
+        }
+        
+        movieService.saveAll(movies);
+        */
+
+        /*
+        JsonNode movies = getMovies(json);
+        for (JsonNode j_movie : movies) {
+            Movie movie = createMovieFromNode(j_movie);
+            this.movieService.saveMovie(movie);
+        }
+        */
+
+
+        List<Movie> movies = Arrays.asList(
+            new Movie(
+                "Inception",
+                Arrays.asList("Action", "Sci-Fi", "Thriller"),
+                "USA",
+                "Warner Bros",
+                2010,
+                148,
+                "PG-13",
+                829895144L,
+                87.0, 1500, // RT
+                8.9, 20000, // LB
+                74.0, 300,  // MC
+                8.8, 2200000, // IMDB
+                8.3, 1000000  // TMDB
+            ),
+            new Movie(
+                "The Dark Knight",
+                Arrays.asList("Action", "Crime", "Drama"),
+                "USA",
+                "Warner Bros",
+                2008,
+                152,
+                "PG-13",
+                1004558444L,
+                94.0, 1800, // RT
+                9.1, 25000, // LB
+                84.0, 500,  // MC
+                9.0, 2500000, // IMDB
+                8.6, 1500000  // TMDB
+            ),
+            new Movie(
+                "Parasite",
+                Arrays.asList("Comedy", "Drama", "Thriller"),
+                "South Korea",
+                "CJ Entertainment",
+                2019,
+                132,
+                "R",
+                263358147L,
+                98.0, 1900, // RT
+                8.6, 18000, // LB
+                90.0, 600,  // MC
+                8.6, 900000, // IMDB
+                8.7, 700000  // TMDB
+            ),
+            new Movie(
+                "Interstellar",
+                Arrays.asList("Adventure", "Drama", "Sci-Fi"),
+                "USA",
+                "Paramount Pictures",
+                2014,
+                169,
+                "PG-13",
+                677471339L,
+                91.0, 1600, // RT
+                8.7, 22000, // LB
+                74.0, 350,  // MC
+                8.7, 1800000, // IMDB
+                8.4, 1200000  // TMDB
+            )
+        );
+
+        movieService.saveAll(movies);
+    }
+
+    private Movie createMovieFromNode(JsonNode j_movie) {
+        JsonNode node = j_movie.get("node");
+
+        String title = node.get("title").asText();
+
+        ArrayList<String> genre = new ArrayList<>(Arrays.asList(node.get("genre").asText().split(",")));
+        
+        String country = node.get("country").asText();
+        String studio = node.get("studio").asText();
+        Integer year = node.get("releaseYear").asInt();
+        int runtime = node.get("runtime").asInt();
+        String rating = node.get("rating").asText();
+        Long box_office = node.get("box_office").asLong();
+
+        Double RTrating = node.get("RTrating").asDouble();
+        int RTnb = node.get("RTnb").asInt();
+        Double LBrating = node.get("LBrating").asDouble();
+        int LBnb = node.get("LBnb").asInt();
+        Double MCrating = node.get("MCrating").asDouble();
+        int MCnb = node.get("MCnb").asInt();
+
+        JsonNode j_imdb = node.get("imdbScore");
+        Double IMDBrating = j_imdb.get("averageScore").asDouble();
+        int IMDBnb = j_imdb.get("numberVotes").asInt();
+
+        Double TMDBrating = node.get("TMDBrating").asDouble();
+        int TMDBnb = node.get("TMDBnb").asInt();
+
+        return new Movie(title, genre, country, studio, year, runtime, rating, box_office,
+            RTrating, RTnb, LBrating, LBnb, MCrating, MCnb, IMDBrating, IMDBnb, TMDBrating, TMDBnb);
+    }
+
+    private JsonNode getMovies(JsonNode json) {
+        return Optional.ofNullable(json)
+            .map(j -> j.get("data"))
+            .orElseThrow(() -> new IllegalArgumentException("Invalid JSON object."));
+    }
     
 }
--- backend/backend/src/main/java/com/example/backend/MovieController.java
+++ backend/backend/src/main/java/com/example/backend/MovieController.java
@@ -1,22 +1,133 @@
 package com.example.backend;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+// import java.util.stream.Collectors;
 
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+// import org.springframework.hateoas.CollectionModel;
+// import org.springframework.hateoas.EntityModel;
+// import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.servlet.function.EntityResponse;
 
-import org.springframework.hateoas.EntityModel;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+// import org.springframework.hateoas.EntityModel;
+// import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
+@RequestMapping("/api/movies")
 public class MovieController {
+    
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Movie>> searchMovies(
+        @RequestParam(required = false) String genre,
+        @RequestParam(required = false) String country,
+        @RequestParam(required = false) String studio,
+        @RequestParam(required = false) String year,
+        @RequestParam(required = false) String rating,
+        @RequestParam(required = false) String min_year,
+        @RequestParam(required = false) String max_year
+    ) {
+        List<Movie> movies = this.movieService.getMovies(genre, country, studio, year, rating, min_year, max_year);
+        if (movies == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(Collections.emptyList());
+        }
+        return ResponseEntity.ok(movies);
+    }
+
+    @GetMapping("/average")
+    public ResponseEntity<Double> searchAverage(
+        @RequestParam(required = false) String genre,
+        @RequestParam(required = false) String country,
+        @RequestParam(required = false) String studio,
+        @RequestParam(required = false) String year,
+        @RequestParam(required = false) String rating,
+        @RequestParam(required = false) String min_year,
+        @RequestParam(required = false) String max_year,
+        @RequestParam(required = true) String platform
+    ) {
+
+        if (!platformExists(platform)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(this.movieService.getAverage(genre, country, studio, year, rating, min_year, max_year, platform));
+    }
+
+    @GetMapping("/allaverages")
+    public ResponseEntity<HashMap<String, Double>> searchAllAverages(
+        @RequestParam(required = false) String genre,
+        @RequestParam(required = false) String country,
+        @RequestParam(required = false) String studio,
+        @RequestParam(required = false) String year,
+        @RequestParam(required = false) String rating,
+        @RequestParam(required = false) String min_year,
+        @RequestParam(required = false) String max_year
+    ) {
+        HashMap<String, Double> movies = this.movieService.getAllAverages(genre, country, studio, year, rating, min_year, max_year);
+        if (movies == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new HashMap<String, Double>());
+        }
+        return ResponseEntity.ok(movies);
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Integer> searchNumberOfReviews(
+        @RequestParam(required = false) String genre,
+        @RequestParam(required = false) String country,
+        @RequestParam(required = false) String studio,
+        @RequestParam(required = false) String year,
+        @RequestParam(required = false) String rating,
+        @RequestParam(required = false) String min_year,
+        @RequestParam(required = false) String max_year,
+        @RequestParam(required = true) String platform
+    ) {
+        if (!platformExists(platform)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(this.movieService.getNumberOfReviews(genre, country, studio, year, rating, min_year, max_year, platform));
+    }
+    
+    
+    @GetMapping("/movie")
+    public ResponseEntity<Movie> searchMovieByName(@RequestParam String title) {
+        Movie movie = this.movieService.getMovieByTitle(title);
+        if (movie == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(movie);
+    }
+
+    @GetMapping("/best")
+    public ResponseEntity<List<Movie>> searchBestMovies(@RequestParam int n) {
+        if (n <= 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(Collections.emptyList());
+        }
+        return ResponseEntity.ok(this.movieService.getBestMovies(n));
+    }
+
+
+    public boolean platformExists(String platform) {
+        String[] platforms = {"RT", "MC", "LB", "IMDB", "TMDB"};
+        return Arrays.asList(platforms).contains(platform);
+    }
     
 }
