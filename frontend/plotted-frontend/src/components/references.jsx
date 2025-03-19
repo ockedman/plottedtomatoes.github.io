12aeb055ed46013cc4d25d2e31f8371d18af7f68
Juels, Titan <115128048+thjuels@users.noreply.github.com>
2025-03-19 11:22:09 +0100

    Added sample text to reference page

--- frontend/plotted-frontend/src/components/references.jsx
+++ frontend/plotted-frontend/src/components/references.jsx
@@ -2,9 +2,99 @@
 const referncesPage = () => {
 
   return (
-    <div>
-      <h3>Refernces</h3>
-    </div>
+    <>
+      <h2>Additional Info</h2>
+      <div>
+        <h3>Refernces and Citations</h3>
+        <div style={{
+          display: "flex",
+          justifyContent: "space-around"
+        }}>
+          <div style={{
+            width : "1/3"
+          }}>
+            <h4>FrontEnd</h4>
+            <ul>
+              <li>YouTube. (n.d.). React Tabs Tutorial | Easy and Customizable Tabs in React [Video]. YouTube. https://www.youtube.com/watch?v=sWVgMcz8Q44</li>
+              <li>Stack Overflow. (n.d.). Stack Overflow - Where Developers Learn, Share, & Build Careers. Retrieved from https://stackoverflow.com/</li>
+              <li>NPM. (n.d.). React Tabs - A Simple, Accessible Tabs Component for React. Retrieved from https://www.npmjs.com/package/react-tabs</li>
+              <li>React. (n.d.). React – A JavaScript Library for Building User Interfaces. Retrieved from https://react.dev/</li>
+              <li>Nivo. (n.d.). Nivo - Dataviz Components for React. Retrieved from https://nivo.rocks/</li>
+              <li>Vite. (n.d.). Vite - The Next Generation Frontend Tooling. Retrieved from https://vite.dev/guide/</li>
+              <li>YouTube. (n.d.). How to Use Vite Instead of Create React App (CRA) for React Projects [Video]. YouTube. https://www.youtube.com/watch?v=7pXkD8RXat0</li>
+              <li>MDN Web Docs. (n.d.). MDN Web Docs - Resources for Developers, by Developers. Retrieved from https://developer.mozilla.org/en-US/</li>
+              <li>GeeksforGeeks. (n.d.). GeeksforGeeks - A Computer Science Portal for Geeks. Retrieved from https://www.geeksforgeeks.org/</li>
+            </ul>
+          </div>
+          <div style={{
+            width : "1/3"
+          }}>
+            <h4>Backend</h4>
+            <ul>
+              <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</li>
+            </ul>
+          </div>
+          <div style={{
+            width : "1/3"
+          }}>
+            <h4>Data Processing</h4>
+            <ul>
+              <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <h3>Work Distribution</h3>
+      <div style={{
+        display: "flex",
+        justifyContent: "space-around"
+      }}>
+        <div>
+          <h4>Titan</h4>
+          <ul>
+            <li>Entire Frontend</li>
+            <li>Assisted with viz Design</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Adam</h4>
+          <ul>
+            <li>..put work here..</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Daniel</h4>
+          <ul>
+            <li>..put work here..</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Yuchen</h4>
+          <ul>
+            <li>..put work here..</li>
+          </ul>
+        </div>
+      </div>
+      <h3>Task Abstraction</h3>
+      <div>
+      <p>....Put the task abstraction of the data here....</p>
+      </div>
+      <h3>Data Abstraction and Provenance</h3>
+      <div>
+      <p>....Put the data abstraction and provenance of the data here....</p>
+      </div>
+      <h3>Source Code</h3>
+      <div>
+        <h3>GitHub Repository</h3>
+        <ul>
+          <li><h4>https://github.com/ockedman/Plotted-Tomatoes</h4></li>
+        </ul>
+      </div>
+      <h3>Source Data</h3>
+      <div>
+      <p>....Put the source of the data here....</p>
+      </div>
+    </>
   );
 };
 export default referncesPage;
